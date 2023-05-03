22e11af870b10c9aa55eb32422424f02147152b4
Johnny <44646589+Junnyyy@users.noreply.github.com>
2023-05-02 19:52:28 -0500

    ✨ Create login component

---
+++ components/layout.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import type { layoutProps } from "../types/types";
+
+const Layout = ({ children, title, nav }: layoutProps) => {
+  return (
+    <>
+      <Head>
+        {title ? <title>{title}</title> : <title>CougarCS - User Portal</title>}
+      </Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+      {nav ? <div>nav</div> : <div>no nav</div>}
+      <div className="flex-1 bg-mainDark">{children}</div>
+    </>
+  );
+};
+
+export default Layout;
