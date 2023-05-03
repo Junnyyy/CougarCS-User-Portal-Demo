6c7b8e93a07ef4cf6e1137195edf6e3ef3306b7b
Johnny <44646589+Junnyyy@users.noreply.github.com>
2023-05-02 19:51:21 -0500

    🐛 Fix color extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,12 +12,8 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      theme: {
-        extend: {
-          colors: {
-            mainDark: "#1C1C1C",
-          },
-        },
+      colors: {
+        mainDark: "#1C1C1C",
       },
     },
   },
